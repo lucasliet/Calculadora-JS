b142f75bc9b49cb18088d590d70ad3273d8b37e2
Wendler Eis <wendler_eis@hotmail.com>
2020-05-10 00:06:53 -0300

    test: adjust calculator tests

--- scripts/tests/calculator.test.js
+++ scripts/tests/calculator.test.js
@@ -1,24 +1,35 @@
+const CalculatorTest = (function () {
+  const {
+    getFirstNumber,
+    setFirstNumber,
+    getSecondNumber,
+    setSecondNumber,
+  } = CalculatorModel;
 
-const CalculatorTest = (function() {
+  const { calculate } = CalculatorController;
+
   function run() {
-    calcular(2, 2, "+") === 4 && calcular(5, 9, "+") === 14
+    setFirstNumber(10);
+    setSecondNumber(10);
+
+    calculate(getFirstNumber(), getSecondNumber(), "+") === 20
       ? console.info("Soma sucesso")
       : console.error("Soma falhou");
-  
-    calcular(4, 2, "-") === 2 && calcular(10, 9, "-") === 1
+
+    calculate(getFirstNumber(), getSecondNumber(), "-") === 0
       ? console.info("Subtrair sucesso")
       : console.error("Subtrair falhou");
-  
-    calcular(3, 2, "*") === 6 && calcular(10, 10, "*") === 100
+
+    calculate(getFirstNumber(), getSecondNumber(), "*") === 100
       ? console.info("Multiplicar sucesso")
       : console.error("Multiplicar falhou");
-  
-    calcular(9, 3, "/") === 3 && calcular(10, 2, "/") === 5
+
+    calculate(getFirstNumber(), getSecondNumber(), "/") === 1
       ? console.info("Dividir sucesso")
       : console.error("Dividir falhou");
   }
 
   return {
-    run
-  }
-})()
+    run,
+  };
+})();
